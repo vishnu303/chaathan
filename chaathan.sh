345cd38d866e6aaa7c02af0dab02f480f1b98ac9
Vishnu M <vishnu.mvk32@gmail.com>
2025-03-15 15:07:25 +0530

    bugfix

--- chaathan.sh
+++ chaathan.sh
@@ -12,7 +12,7 @@ RESET=$(tput sgr0)
 # variables
 TOOLS_DIR="$HOME/tools"
 VENV_DIR="$HOME/bugbounty_venv"
-GO_VERSION="1.21.7"
+GO_VERSION="1.24.1"
 LOG_FILE="$HOME/chaathan_install.log"
 APT_PACKAGES="apt-transport-https curl git jq ruby-full build-essential libcurl4-openssl-dev libssl-dev libxml2-dev libxslt1-dev libgmp-dev zlib1g-dev libffi-dev python3-dev python3-pip npm nmap perl parallel ffuf wfuzz"
 
@@ -47,7 +47,7 @@ setup_environment() {
         OS="unknown"
         log "${RED}Cannot detect OS, defaulting to system Python${RESET}"
     fi
-
+    
     if [ "$OS" = "ubuntu" ]; then
         log "${GREEN}[+] Setting up virtual environment for Ubuntu${RESET}"
         execute "python3 -m venv $VENV_DIR" "Virtual environment creation" || exit 1
@@ -85,21 +85,21 @@ clone_and_setup() {
     fi
 }
 
-# Display banner 
+# Display banner
 banner() {
     echo "${RED} ######################################################### ${RESET}"
     echo "${RED} #                 TOOLS FOR BUG BOUNTY                  # ${RESET}"
     echo "${RED} ######################################################### ${RESET}"
     echo "${BLUE}
-              
-      _____ _    _                 _______ _    _          _   _  
-     / ____| |  | |   /\        /\|__   __| |  | |   /\   | \ | | 
-    | |    | |__| |  /  \      /  \  | |  | |__| |  /  \  |  \| | 
-    | |    |  __  | / /\ \    / /\ \ | |  |  __  | / /\ \ |     | 
-    | |____| |  | |/ ____ \  / ____ \| |  | |  | |/ ____ \| |\  | 
-     \_____|_|  |_/_/    \_\/_/    \_\_|  |_|  |_/_/    \_\_| \_| 
-                                                                                                                             
- ${RESET}"
+
+      _____ _    _                 _______ _    _          _   _
+     / ____| |  | |   /\        /\|__   __| |  | |   /\   | \ | |
+    | |    | |__| |  /  \      /  \  | |  | |__| |  /  \  |  \| |
+    | |    |  __  | / /\ \    / /\ \ | |  |  __  | / /\ \ |     |
+    | |____| |  | |/ ____ \  / ____ \| |  | |  | |/ ____ \| |\  |
+     \_____|_|  |_/_/    \_\/_/    \_\_|  |_|  |_/_/    \_\_| \_|
+
+    ${RESET}"
     echo "${GREEN} Tools created by the best people in the InfoSec Community ${RESET}"
     echo "${GREEN}                   Thanks to everyone!                     ${RESET}"
 }
@@ -108,28 +108,36 @@ banner() {
 main() {
     banner
     setup_environment
-
+    
     # Update system and install base dependencies
     execute "sudo apt -y update && sudo apt -y upgrade" "System update/upgrade" || exit 1
     install_packages apt "$APT_PACKAGES" "Base dependencies installation" || exit 1
     execute "curl -sL https://git.io/vokNn | sudo bash -" "apt-fast installation" || exit 1
-
+    
     # Install Go
     if [ ! -d "/usr/local/go" ]; then
-        execute "wget -q https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz -O go.tar.gz && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz" "Go installation" || exit 1
-        echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bash_profile
-        export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+        execute "wget -q https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz -O go.tar.gz" "Downloading Go" || exit 1
+        execute "sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz" "Extracting Go" || exit 1
+        echo '# Set Go environment variables
+        export GOROOT=/usr/local/go
+        export GOPATH=$HOME/go
+        export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' > ~/.bash_profile
+        export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin  # Immediate effect in this session
     else
         log "${BLUE}Golang is already installed${RESET}"
     fi
-    source ~/.bash_profile
-
+    if [ -f ~/.bash_profile ]; then
+        source ~/.bash_profile
+    else
+        log "${RED}Warning: ~/.bash_profile not found, PATH not updated${RESET}"
+    fi
+    
     # Create tools directory
     mkdir -p "$TOOLS_DIR" && cd "$TOOLS_DIR" || exit 1
-
+    
     # Install tools
     log "${RED} #################### Installing Tools #################### ${RESET}"
-
+    
     # Basic Tools
     install_packages pip "py-altdns" "py-altdns installation"
     install_packages apt "nmap sqlmap" "nmap and sqlmap installation"
@@ -138,12 +146,12 @@ main() {
     install_packages go "github.com/harleo/knockknock" "knockknock installation"
     clone_and_setup "https://github.com/yassineaboukir/asnlookup.git" "asnlookup" "$PIP_CMD install -r requirements.txt"
     install_packages go "github.com/j3ssie/metabigor" "metabigor installation"
-
+    
     # Fuzzing Tools
     install_packages go "github.com/OJ/gobuster/v3" "gobuster installation"
     install_packages go "github.com/ffuf/ffuf" "ffuf installation"
     install_packages pip "dirsearch" "dirsearch installation"
-
+    
     # Domain Enum Tools
     clone_and_setup "https://github.com/nsonaniya2010/SubDomainizer.git" "SubDomainizer" "$PIP_CMD install -r requirements.txt"
     clone_and_setup "https://github.com/eldraco/domain_analyzer.git" "domain_analyzer"
@@ -153,18 +161,18 @@ main() {
     clone_and_setup "https://github.com/aboul3la/Sublist3r.git" "Sublist3r" "$PIP_CMD install -r requirements.txt"
     install_packages go "github.com/Ice3man543/SubOver" "SubOver installation"
     install_packages pip "spyse.py" "spyse.py installation"
-
+    
     # CORS Tools
     clone_and_setup "https://github.com/s0md3v/Corsy.git" "corsy" "$PIP_CMD install -r requirements.txt"
     clone_and_setup "https://github.com/chenjj/CORScanner.git" "corscanner" "$PIP_CMD install -r requirements.txt"
     install_packages go "github.com/Tanmay-N/CORS-Scanner" "CORS-Scanner installation"
-
+    
     # XSS Tools
     clone_and_setup "https://github.com/hahwul/dalfox.git" "dalfox" "go build && sudo cp dalfox /usr/local/bin/"
     clone_and_setup "https://github.com/s0md3v/XSStrike.git" "XSStrike" "$PIP_CMD install -r requirements.txt"
     install_packages gem "XSpear colorize selenium-webdriver terminal-table progress_bar" "XSpear and dependencies"
     clone_and_setup "https://github.com/M4cs/traxss.git" "traxss" "$PIP_CMD install -r requirements.txt"
-
+    
     # Cloud Workflow Tools
     execute "curl -s https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip && unzip -q awscliv2.zip && sudo ./aws/install && rm -rf aws awscliv2.zip" "AWS CLI installation"
     clone_and_setup "https://github.com/gwen001/s3-buckets-finder.git" "s3-buckets-finder"
@@ -175,7 +183,7 @@ main() {
     clone_and_setup "https://github.com/greycatz/CloudUnflare.git" "CloudUnflare"
     clone_and_setup "https://github.com/fellchase/flumberboozle.git" "flumberboozle"
     clone_and_setup "https://github.com/RhinoSecurityLabs/GCPBucketBrute.git" "GCPBucketBrute" "$PIP_CMD install -r requirements.txt"
-
+    
     # CMS Tools
     clone_and_setup "https://github.com/Dionach/CMSmap.git" "CMS/CMSmap" "$PIP_CMD install ."
     clone_and_setup "https://github.com/jekyc/wig.git" "CMS/wig" "$PYTHON_CMD setup.py install"
@@ -184,13 +192,13 @@ main() {
     install_packages pip "droopescan" "droopescan installation"
     clone_and_setup "https://github.com/immunIT/drupwn.git" "CMS/drupwn" "$PYTHON_CMD setup.py install"
     clone_and_setup "https://github.com/0ang3el/aem-hacker.git" "CMS/aem-hacker"
-
+    
     # Git Tools
     clone_and_setup "https://github.com/HightechSec/git-scanner.git" "GIT/git-scanner" "chmod +x gitscanner.sh"
     clone_and_setup "https://github.com/hisxo/gitGraber.git" "GIT/gitGraber" "$PIP_CMD install -r requirements.txt"
     clone_and_setup "https://github.com/tillson/git-hound.git" "GIT/git-hound" "go build -o githound main.go && sudo cp githound /usr/local/bin/"
     clone_and_setup "https://github.com/gwen001/github-search.git" "GIT/github-search" "$PIP_CMD install -r requirements.txt"
-
+    
     # Frameworks
     clone_and_setup "https://github.com/1N3/Sn1per.git" "Frameworks/Sn1per"
     clone_and_setup "https://github.com/j3ssie/Osmedeus.git" "Frameworks/osmedeus"
@@ -199,7 +207,7 @@ main() {
     clone_and_setup "https://github.com/1N3/BlackWidow.git" "Frameworks/BlackWidow"
     clone_and_setup "https://github.com/screetsec/Sudomy.git" "Frameworks/Sudomy" "$PIP_CMD install -r requirements.txt && sudo npm i -g wappalyzer"
     execute "wget -q https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux -O findomain && chmod +x findomain && sudo mv findomain /usr/local/bin/" "findomain installation"
-
+    
     # Other Tools
     clone_and_setup "https://github.com/swisskyrepo/SSRFmap.git" "SSRFMap" "$PIP_CMD install -r requirements.txt"
     install_packages pip "xsrfprobe" "xsrfprobe installation"
@@ -218,18 +226,18 @@ main() {
     install_packages go "github.com/jaeles-project/gospider" "gospider installation"
     clone_and_setup "https://github.com/GerbenJavado/LinkFinder.git" "LinkFinder" "$PIP_CMD install -r requirements.txt && $PYTHON_CMD setup.py install"
     clone_and_setup "https://github.com/m4ll0k/SecretFinder.git" "SecretFinder" "$PIP_CMD install -r requirements.txt"
-
+    
     # Wordlists
     for repo in "assetnote/commonspeak2-wordlists" "fuzzdb-project/fuzzdb" "1N3/IntruderPayloads" "swisskyrepo/PayloadsAllTheThings" "danielmiessler/SecLists"; do
         clone_and_setup "https://github.com/$repo.git" "Wordlists/$(basename $repo)"
     done
     cd Wordlists/SecLists/Discovery/DNS && execute "cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt" "Cleaning jhaddix DNS list" && cd ../../..
-
+    
     # Tomnomnom Tools
     for tool in meg assetfinder waybackurls gf httprobe "hacks/concurl" unfurl "hacks/anti-burl" "hacks/filter-resolved" qsreplace; do
         install_packages go "github.com/tomnomnom/$tool" "$tool installation"
     done
-
+    
     # Additional Tools
     install_packages pip "arjun bbot" "arjun and bbot installation"
     install_packages go "github.com/dwisiswant0/cf-check" "cf-check installation"
@@ -240,12 +248,12 @@ main() {
     install_packages go "github.com/projectdiscovery/pdtm/cmd/pdtm" "pdtm installation"
     cd ~/go/bin && execute "./pdtm --install-all" "Installing pdtm tools" && cd ..
     clone_and_setup "https://github.com/xnl-h4ck3r/waymore.git" "waymore" "$PYTHON_CMD setup.py install"
-
+    
     # Instructions
     log "${GREEN}Use 'source ~/.bash_profile' to enable shell functions${RESET}"
     log "${GREEN}ALL THE TOOLS ARE MADE BY THE BEST PEOPLE OF THE INFOSEC COMMUNITY${RESET}"
     log "${GREEN}                I AM JUST A SCRIPT-KIDDIE ;)                 ${RESET}"
-
+    
     # Deactivate virtual environment if on Ubuntu
     [ "$OS" = "ubuntu" ] && deactivate
 }
